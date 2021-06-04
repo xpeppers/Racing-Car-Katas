4949a951b1632f046ec8c31fa372171dbdb89c7a
Imer10 <imer.rota@gmail.com>
2021-06-04 17:32:56 +0200

    ILineReader

--- CSharp/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
+++ CSharp/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
@@ -33,7 +33,47 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
                 return html;
             }
         }
+
+        public string ConvertToHtmlWithLineReader()
+        {
+            ILineReader reader = new FileLineReader(_fullFilenameWithPath);
+            using (TextReader unicodeFileStream = File.OpenText(_fullFilenameWithPath))
+            {
+                string html = string.Empty;
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    html += HttpUtility.HtmlEncode(line);
+                    html += "<br />";
+                    line = reader.ReadLine();
+                }
+
+                return html;
+            }
+        }
     }
+
+    internal class FileLineReader : ILineReader
+    {
+        private string fullFilenameWithPath;
+
+        public FileLineReader(string fullFilenameWithPath)
+        {
+            this.fullFilenameWithPath = fullFilenameWithPath;
+        }
+
+        public string ReadLine()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal interface ILineReader
+    {
+        string ReadLine();
+    }
+
     class HttpUtility
     {
         public static string HtmlEncode(string line)
