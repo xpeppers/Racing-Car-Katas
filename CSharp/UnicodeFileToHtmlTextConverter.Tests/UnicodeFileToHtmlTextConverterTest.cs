f8c12f14b5c9810a1d517654aea6e0ab7a34e153
lerede <lerede@outlook.com>
2021-06-04 17:10:32 +0200

    Check_Exception_With_An_Empty_String

--- CSharp/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
+++ CSharp/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
@@ -1,14 +1,15 @@
 
+using System;
 using Xunit;
 namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
 {
     public class HikerTest
     {
         [Fact]
-        public void Foobar()
+        public void Check_Exception_With_An_Empty_String()
         {
-            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter("foobar.txt");
-            Assert.Equal("fixme", converter.GetFilename());
+            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(string.Empty);
+            Assert.Throws< ArgumentException>(() => converter.ConvertToHtml());
         }
     }
 }
