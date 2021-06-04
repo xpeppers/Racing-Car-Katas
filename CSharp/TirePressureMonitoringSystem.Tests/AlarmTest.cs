490db283cc7dc01376c2cd96afd22a30e99bd72c
Gigivi <luigi.calabrese@aptos.com>
2021-06-04 16:56:58 +0200

    Extended test inline data

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -6,8 +6,16 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
 	{
 		[Theory]
 		[InlineData(0, true)]
+		[InlineData(16, true)]
 		[InlineData(17, false)]
 		[InlineData(18, false)]
+		[InlineData(19, false)]
+		[InlineData(20, false)]
+		[InlineData(21, false)]
+		[InlineData(22, true)]
+		[InlineData(23, true)]
+		[InlineData(24, true)]
+		[InlineData(25, true)]
 		public void TestAlarm(double value, bool expected)
 		{
 			SensorStub st = new SensorStub(value);
