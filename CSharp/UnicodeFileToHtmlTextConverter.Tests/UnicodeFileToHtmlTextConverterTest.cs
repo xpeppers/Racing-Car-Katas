f74804b8382b3f1062fe7317902511d0ca118006
stefanotriulzi <stefano.triulzi@aptos.com>
2021-06-04 17:33:34 +0200

    Added Empty Test

--- CSharp/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
+++ CSharp/UnicodeFileToHtmlTextConverter.Tests/UnicodeFileToHtmlTextConverterTest.cs
@@ -1,4 +1,5 @@
 
+using System.IO;
 using Xunit;
 namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
 {
@@ -11,7 +12,15 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
             Assert.Equal("foobar.txt", converter.GetFilename());
         }
 
+        [Fact]
+        public void ConvertFileName()
+        {
+            File.WriteAllText("foobar.txt", string.Empty);
 
+            UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter("foobar.txt");
+            string result = converter.ConvertToHtml();
+            Assert.Equal(string.Empty, result);
+        }
 
     }
 }
