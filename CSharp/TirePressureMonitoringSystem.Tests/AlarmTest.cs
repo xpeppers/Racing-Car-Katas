edf4a3197ecc50901f2fc84b17958ed3399dec84
lerede <lerede@outlook.com>
2021-06-04 16:49:33 +0200

    Swirched test to the new controlled sensor

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -39,6 +39,28 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
         }
 
         [Fact]
+        public void Check_Alarm_Off_pressure_into_range_with_controlled_sensor()
+        {
+            ISensor testableSensor = new TestableSensor(20.7);
+
+            Alarm alarm = new Alarm(testableSensor);
+            alarm.Check();
+
+            Assert.False(alarm.AlarmOn);
+        }
+
+        [Fact]
+        public void Check_Alarm_On_below_threshold_with_controlled_sensor()
+        {
+            ISensor testableSensor = new TestableSensor(15.8);
+
+            Alarm alarm = new Alarm(testableSensor);
+            alarm.Check();
+
+            Assert.True(alarm.AlarmOn);
+        }
+
+        [Fact]
         public void Check_Alarm_On_above_threshold_with_controlled_sensor()
         {
             const double fixedPressure = 21.1d;
