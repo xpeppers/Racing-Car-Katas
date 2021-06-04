4f4a9ab38a8df428d6c22f24f211aa31e2141d5d
ironzani <ivan.ronzani@aptos.com>
2021-06-04 16:34:21 +0200

    Aded two test cases: 17 and 18

--- CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
+++ CSharp/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -2,33 +2,51 @@ using Xunit;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystem
 {
-    public class AlarmTest
-    {
+	public class AlarmTest
+	{
 
-        [Fact]
-        public void TestAlarmOnSensorValue0()
-        {
-            SensorStub st = new SensorStub(0);
+		[Fact]
+		public void TestAlarmOnSensorValue0()
+		{
+			SensorStub st = new SensorStub(0);
 
-            Alarm alarm = new Alarm(st);
-            Assert.False(alarm.AlarmOn);
-        }
+			Alarm alarm = new Alarm(st);
+			alarm.Check();
+			Assert.True(alarm.AlarmOn);
+		}
 
+		[Fact]
+		public void TestAlarmOnSensorValue17()
+		{
+			SensorStub st = new SensorStub(17);
 
-    }
+			Alarm alarm = new Alarm(st);
+			alarm.Check();
+			Assert.False(alarm.AlarmOn);
+		}
 
-    class SensorStub : ISensor
-    {
-        public double Value { get; set; }
+		[Fact]
+		public void TestAlarmOnSensorValue18()
+		{
+			SensorStub st = new SensorStub(18);
+			Alarm alarm = new Alarm(st);
+			alarm.Check();
+			Assert.False(alarm.AlarmOn);
+		}
+	}
 
-        public SensorStub(double value)
-        {
-            Value = value;
-        }
+	class SensorStub : ISensor
+	{
+		public double Value { get; set; }
 
-        public double PopNextPressurePsiValue()
-        {
-            return Value;
-        }
-    }
+		public SensorStub(double value)
+		{
+			Value = value;
+		}
+
+		public double PopNextPressurePsiValue()
+		{
+			return Value;
+		}
+	}
 }
